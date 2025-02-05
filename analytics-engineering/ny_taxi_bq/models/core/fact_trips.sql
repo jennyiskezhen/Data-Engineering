d34eec165315f5ea974ab9b031401444497c4b2b
jenkzwang@gmail.com <jenkzwang@gmail.com>
2025-02-05 09:50:02 +0000

    add comment

--- analytics-engineering/ny_taxi_bq/models/core/fact_trips.sql
+++ analytics-engineering/ny_taxi_bq/models/core/fact_trips.sql
@@ -56,4 +56,5 @@ inner join dim_zones as pickup_zone
 on trips_unioned.pickup_locationid = pickup_zone.locationid
 inner join  dim_zones as dropoff_zone
 on trips_unioned.dropoff_locationid = dropoff_zone.locationid
+-- to join the two tables
 
